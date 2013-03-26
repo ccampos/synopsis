bdd10d0a7bdfbd395cb56eb2862771f7eb66ab10
Claudio Campos <claudio@claudiocampos.com>
2013-03-26 06:03:56 -0400

    add routes

---
+++ foo.js
@@ -0,0 +1,5 @@
+var _ = require('underscore');
+
+_.each([1, 2, 3], function(value) {
+	console.log('console prints', value);
+});
--- server.js
+++ server.js
@@ -6,11 +6,11 @@ function start() {
 	function onRequest(req, resp) {
 		console.log('Request received.');
 		resp.writeHead(200, {'Content-Type': 'text/plain'});
-		resp.write('Hello World');
+		resp.write('I am programming in Node.js!');
 		resp.end();
 	};	
 
-	http.createServer(onRequest).listen(8124);
+	http.createServer(onRequest).listen(8889);
 
 	console.log('Server has started.');
 };
